a103ec764d213dc0367492dd030a410766744b8b
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-08-05 22:30:24 +0400

    zadacha done

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+//Например: 
+//4 -> 16
+//-3 -> 9 
+//-7 -> 49
+
+Console.Write("Vvedite tseloe chislo: ");
+int num = Convert.ToInt32(Console.ReadLine()); //-2 147 483 648 to 2 147 483 647
+int square = num * num;
+Console.Write($"Kvadrat chisla {num} = {square}");
+
