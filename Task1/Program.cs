906da8acf983f174c6c2338cfba2684ab48c27da
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-08-06 12:39:21 +0400

    1st v processe

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,4 +5,15 @@
 //a = 9, b = -3 -> да 
 //a = -3 b = 9 -> нет
 
-Console.write("vvedite pervoe chislo")
+Console.WriteLine("vvedite pervoe chislo");
+int a = Convert.ToInt32(Console.Readline());
+Console.Writeline("vvedite vtoroe chislo");
+int b = Convert.ToInt32(Console.Readline());
+if (b*b==a)
+{
+Console.WriteLine("YAVLYAETSYA");
+}
+else 
+{
+Console.WriteLine("Ne yavlaetsa kvadratom");
+}
