0a1ea42f5e9b30b48d30dcf21272a3b185e6e02b
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-08-06 14:13:43 +0400

    zadacha 4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Vvedite trehznachnoe chislo");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 100 || a < 1000)
+{
+Console.WriteLine($"Last digit of the number {a}={a % 10}");
+}
+else
+Console.WriteLine("Vveli ne trehznachnoe, repeat");
